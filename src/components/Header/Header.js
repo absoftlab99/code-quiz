ee03f936eefe0f474be3e094c77be8531ebcef6c
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-10-11 10:12:40 +0600

    Header Completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import { Link } from 'react-router-dom';
+import {Nav, Navbar, Container} from 'react-bootstrap';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar bg="primary" expand="md">  
+            <Container>  
+            <Navbar.Brand to="/">Navbar Brand</Navbar.Brand>  
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />  
+            <Navbar.Collapse id="basic-navbar-nav">  
+                <Nav className="ms-auto menu">
+                    <Link className='text-light active' to='/home'>Home</Link>
+                    <Link className='text-light' to='/topics'>Topics</Link>
+                    <Link className='text-light' to='/statistics'>Statistics</Link>
+                    <Link className='text-light' to='/blog'>Blog</Link>
+                </Nav>  
+            </Navbar.Collapse>  
+            </Container>  
+        </Navbar>
     );
 };
 
