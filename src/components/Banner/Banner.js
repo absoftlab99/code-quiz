230d2ef140a2ce20427c895f7205ae447fc12206
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-10-11 13:06:21 +0600

    banner completed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,15 @@ import './Banner.css';
 const Banner = () => {
     return (
         <div className='banner'>
-            
+            <div className="row">
+                <div className="col-6">
+                    <h1 className='display-2 text-start p-5 fw-bolder text-light'>
+                        Programming is a <span className='text-warning'>Poitry</span>
+                    </h1>
+                    <button className='btn btn-warning'>Explore Quizes</button>
+                </div>
+                <div className="col-6"></div>
+            </div>
         </div>
     );
 };
